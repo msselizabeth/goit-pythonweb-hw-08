dae64b77126f2b270f48e2bb7774b3ebb25fa015
Yelyzaveta Sheremet <elizabethsheremet@gmail.com>
2026-06-06 15:55:06 -0400

    added routes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from app.db_connection import session_manager
+from app.api.contacts import router as contacts_router
 
-app=FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    await session_manager.close()
+
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(contacts_router, prefix="/api")
+
 
 @app.get("/api/health")
-def root():
-    return {"message": "Welcome to FastAPI!"}
+async def healthcheck():
+    return {"status": "ok"}
