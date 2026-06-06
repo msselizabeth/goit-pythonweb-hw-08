cfd6fbab80fd10aa1b1b142b69d63cbb1dcb7574
Yelyzaveta Sheremet <elizabethsheremet@gmail.com>
2026-06-06 15:33:17 -0400

    service for contacts

---
+++ app/service.py
@@ -0,0 +1,43 @@
+from repository import ContactRepository
+from schemas import ContactModel
+from fastapi import HTTPException
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
+
+
+class ContactService:
+    def __init__(self, repository: ContactRepository):
+        self.repository = repository
+
+    async def get_contacts(self, skip: int, limit: int):
+        return await self.repository.get_contacts(skip, limit)
+
+    async def get_contact(self, id: int):
+        contact = await self.repository.get_contact_by_id(id)
+        if contact is None:
+            raise HTTPException(status_code=404, detail="Contact not found")
+        return contact
+
+    async def create_contact(self, body: ContactModel):
+        try:
+            return await self.repository.create_contact(body)
+        except IntegrityError:
+            raise HTTPException(
+                status_code=409, detail="Contact with this email already exists"
+            )
+        except SQLAlchemyError:
+            raise HTTPException(status_code=500, detail="Database error")
+
+    async def update_contact(self, id: int, body: ContactModel):
+        try:
+            contact = await self.repository.update_contact(id, body)
+            if contact is None:
+                raise HTTPException(status_code=404, detail="Contact not found")
+            return contact
+        except SQLAlchemyError:
+            raise HTTPException(status_code=500, detail="Database error")
+
+    async def delete_contact(self, id: int):
+        contact = await self.repository.delete_contact(id)
+        if contact is None:
+            raise HTTPException(status_code=404, detail="Contact not found")
+        return contact
