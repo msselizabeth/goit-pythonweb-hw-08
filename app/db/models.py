48acae3e7d2bd0de26ff900daa4c8dc57beaaba3
Yelyzaveta Sheremet <elizabethsheremet@gmail.com>
2026-06-07 21:39:31 -0400

    added User model

--- app/db/models.py
+++ app/db/models.py
@@ -1,5 +1,5 @@
 from datetime import date
-from sqlalchemy import String, Integer, Date
+from sqlalchemy import String, Integer, Date, Boolean
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 class Base(DeclarativeBase):
@@ -12,7 +12,15 @@ class Contact(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     first_name: Mapped[str] = mapped_column(String(50), nullable=False)
     last_name: Mapped[str] = mapped_column(String(50), nullable=False)
-    email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
     phone: Mapped[str] = mapped_column(String(15), nullable=False)
     birthday: Mapped[date] = mapped_column(Date, nullable=False)
     additional_data: Mapped[str | None] = mapped_column(String, nullable=True)
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(255), unique=True, index=True)
+    password: Mapped[str] = mapped_column(String(255))
+    avatar_url: Mapped[str | None] = mapped_column(String(255), nullable=True)
+    is_verified: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
