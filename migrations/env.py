be01680749cc108782337f427a47d3b868999154
Yelyzaveta Sheremet <elizabethsheremet@gmail.com>
2026-06-13 21:39:57 -0400

    updated env

--- migrations/env.py
+++ migrations/env.py
@@ -2,6 +2,8 @@ import os
 import asyncio
 from logging.config import fileConfig
 
+
+
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
@@ -10,16 +12,19 @@ from alembic import context
 
 from app.db.models import Base
 
+# ENV
+from app.config import settings
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
-database_url = os.environ.get("DATABASE_URL")
-if database_url: 
-    config.set_main_option("sqlalchemy.url", database_url)
+# database_url = settings.DATABASE_URL
+# if database_url: 
+#     config.set_main_option("sqlalchemy.url", database_url)
 
 # Interpret the config file for Python logging.
-# This line sets up loggers basically.
+# This line sets up loggers basically. 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
@@ -71,9 +76,11 @@ async def run_async_migrations() -> None:
     and associate a connection with the context.
 
     """
+    ini_section = config.get_section(config.config_ini_section, {})
+    ini_section["sqlalchemy.url"] = settings.DATABASE_URL
 
     connectable = async_engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        ini_section,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
